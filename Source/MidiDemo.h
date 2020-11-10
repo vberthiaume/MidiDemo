a6d006d6ed0587a48a7bbf853237df3d61998988
Vincent Berthiaume <vberthiaume@gmail.com>
2020-11-10 08:23:35 -0500

    put back note ons

--- Source/MidiDemo.h
+++ Source/MidiDemo.h
@@ -134,51 +134,63 @@ public:
         updateDeviceList (false);
     }
 
+    int i = -1;
+
     void handleNoteOn(MidiKeyboardState *, int midiChannel, int midiNoteNumber, float velocity) override
     {
-#if 0
+#if 1
         MidiMessage m (MidiMessage::noteOn (midiChannel, midiNoteNumber, velocity));
         m.setTimeStamp (Time::getMillisecondCounterHiRes() * 0.001);
         sendToOutputs (m);
 #else
         switch (++i)
         {
-        case 0:
-        case 1:
-        case 2:
-        {
-            DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request system info");
-            //get sys info  f0    2d    7c    08    00    40    40    40    00    00    02    44    03    00    f7
-            MidiMessage m(0xf0, 0x2d, 0x7c, 0x08, 0x00, 0x40, 0x40, 0x40, 0x00, 0x00, 0x02, 0x44, 0x03, 0x00, 0xf7);
-            m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
-            sendToOutputs(m);
-            break;
-        }
-        case 3:
-        case 4:
-        case 5:
-        {
-            DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request active mode");
-            //get cur mode  f0    2d    7c    38    00    40    40    40    00    00    06    40    6d    04    f7
-            MidiMessage m(0xf0, 0x2d, 0x7c, 0x38, 0x00, 0x40, 0x40, 0x40, 0x00, 0x00, 0x06, 0x40, 0x6d, 0x04, 0xf7);
-            m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
-            sendToOutputs(m);
-            break;
-        }
-        case 6:
-        case 7:
-        case 8:
-        {
-            DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request tuning");
-            //f0    2d    7c    08    00    40    40    40    01    00    14    58    03    0e    f7
-            MidiMessage m(0xf0, 0x2d, 0x7c, 0x08, 0x00, 0x40, 0x40, 0x40, 0x01, 0x00, 0x14, 0x58, 0x03, 0x0e, 0xf7);
-            m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
-            sendToOutputs(m);
-            break;
-        }
+            case 0:
+            case 1:
+            case 2:
+            {
+                DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request system info");
+                //get sys info  f0    2d    7c    08    00    40    40    40    00    00    02    44    03    00    f7
+                MidiMessage m(0xf0, 0x2d, 0x7c, 0x08, 0x00, 0x40, 0x40, 0x40, 0x00, 0x00, 0x02, 0x44, 0x03, 0x00, 0xf7);
+                m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
+                sendToOutputs(m);
+                break;
+            }
+            case 3:
+            case 4:
+            case 5:
+            {
+                DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request active mode");
+                //get cur mode  f0    2d    7c    38    00    40    40    40    00    00    06    40    6d    04    f7
+                MidiMessage m(0xf0, 0x2d, 0x7c, 0x38, 0x00, 0x40, 0x40, 0x40, 0x00, 0x00, 0x06, 0x40, 0x6d, 0x04, 0xf7);
+                m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
+                sendToOutputs(m);
+                break;
+            }
+            case 6:
+            case 7:
+            case 8:
+            {
+                DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request tuning");
+                //f0    2d    7c    08    00    40    40    40    01    00    14    58    03    0e    f7
+                MidiMessage m(0xf0, 0x2d, 0x7c, 0x08, 0x00, 0x40, 0x40, 0x40, 0x01, 0x00, 0x14, 0x58, 0x03, 0x0e, 0xf7);
+                m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
+                sendToOutputs(m);
+                break;
+            }
+            case 9:
+            case 10:
+            case 11:
+            {
+                DBG("JUCEEEEEEEEEEEEEEEEEEEEEEEEE request serial number");
+                              //f0    2d    7c    7c    01    40    40    40    00    00    0c    40    6b    0a    f7
+                MidiMessage m(0xf0, 0x2d, 0x7c, 0x7c, 0x01, 0x40, 0x40, 0x40, 0x00, 0x00, 0x0c, 0x40, 0x6b, 0x0a, 0xf7);
+                m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
+                sendToOutputs(m);
+            }
         };
-
-        if (i == 8)
+        
+        if (i == 11)
             i = -1;
 #endif
     }
